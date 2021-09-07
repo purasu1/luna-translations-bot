7237928a431491a28cc84c9a7f2b6d4d833a62fb
Tam CARRE <kokone@takat.su>
2021-09-07 21:44:45 +0200

    Delete build directory

--- build/core/events/guildCreate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.guildCreate = void 0;
-const helpers_1 = require("../../helpers");
-function guildCreate(guild) {
-    (0, helpers_1.log)(`${guild.name} (${guild.id}) added the bot. (Owner: ${guild.ownerId})`);
-}
-exports.guildCreate = guildCreate;
-//# sourceMappingURL=guildCreate.js.map
--- build/testCommunity.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const communityEmitter_1 = require("./modules/community/communityEmitter");
-communityEmitter_1.communityEmitter.on('post', post => console.log(post));
-//# sourceMappingURL=testCommunity.js.map
