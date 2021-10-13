fe9b430843d46bbe97934c3dc25f5ea435ff6ee1
Tam CARRE <kokone@takat.su>
2021-10-13 08:39:15 +0200

    debug

--- src/modules/livechat/chatRelayer.ts
+++ src/modules/livechat/chatRelayer.ts
@@ -96,7 +96,6 @@ function setupLive (frame: DexFrame) {
         isV: msg.is_vtuber
       }
       const tasks = await processComments (frame, [cmt], allEntries)
-      console.log(tasks.filter(t => t._tag === 'SendMessageTask').map((t: any) => `[${t.vId} | ${t.cid} | ${t.content}]`))
       tasks.forEach (runTask)
     }
     else if (msg.type === 'end') {
